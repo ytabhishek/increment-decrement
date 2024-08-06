208d9d023c7199f5c7a608c5c7103fa479759ab3
Abhishek Pratap Singh <ytabhishek721@gmail.com>
2024-08-06 14:30:53 +0530

    Add files via upload

---
+++ increment_decrement.java
@@ -0,0 +1,17 @@
+package Basics;
+
+public class increment_decrement {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(x);
+        x++; //increment operator
+        System.out.println(x);
+        x--;  //decrement operator
+        System.out.println(x);
+
+        // x++ matlb pahle print karo fir add
+        // ++x matlb pahle add karo fir print
+        // x-- matlb pahle print karo fir subtract
+        // --x matlb pahle subtract karo fir print
+    }
+}
